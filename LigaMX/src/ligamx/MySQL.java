aac181cbad96a6c23a85bc249d51372a21d9e380
Elias <eliasabraham6464@gmail.com>
2018-04-03 21:14:18 -0500

    Algunos metodos para alterar registros

--- LigaMX/src/ligamx/MySQL.java
+++ LigaMX/src/ligamx/MySQL.java
@@ -7,9 +7,11 @@ package ligamx;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.LinkedList;
 import javax.swing.JOptionPane;
 
 /**
@@ -20,8 +22,9 @@ public class MySQL {
     private Connection connection=null;
     private Statement comand=null;
     private ResultSet row=null;
+    private PreparedStatement ps=null;
     
-    public Connection Connect(){
+    public void Connect(){
         try{
             Class.forName("com.mysql.jdbc.Driver");
             String servidor = "jdbc:mysql://localhost:3306/ligaMX";
@@ -40,13 +43,68 @@ public class MySQL {
             JOptionPane.showMessageDialog(null, ex, "Error en la conexión a la base de datos: " + ex.getMessage(), JOptionPane.ERROR_MESSAGE);
             connection = null;
         }
-        if(connection != null){
-            System.out.print("Conexion exitosa");
-        }
-        return connection;
     }
+    
+    private void getData(String com){
+        try{
+            comand=connection.createStatement();
+            row=comand.executeQuery(com);
+        }catch(SQLException e){
+            row=null;
+        }
+    }
+    private void action(String comm){
+        try{
+        ps=connection.prepareStatement(comm);
+        ps.execute();
+        }
+        catch(SQLException e){}
+    }
+    
+    public ResultSet getRows(String table){
+        getData("SELECT * FROM "+table+";");
+        return row;
+    }
+    
+    public ResultSet getTables(){
+        getData("SHOW TABLES;");
+        return row;
+    }
+    
+    public ResultSet getDesc(String table){
+        getData("DESCRIBE "+table+";");
+        return row;
+    }
+    
+    public void deleteRow(String table,int id){
+        action("DELETE FROM "+table+" WHERE id"+table+"="+id+";");
+    }
+    
+    public void CloseConnection(){
+        try{
+        connection.close();
+        }
+        catch(SQLException e){}
+    }
+    
     public static void main(String args[]){
         MySQL mysql=new MySQL();
         mysql.Connect();
+        ResultSet tablas=mysql.getTables();
+        ResultSet desc=mysql.getDesc("partido");
+        ResultSet rows=mysql.getRows("tipopartido");
+        //mysql.deleteRow("temporada", 2);
+        try{
+            while (tablas.next()) {
+                System.out.println(""+tablas.getString(1));
+            }
+            while (rows.next()) {
+                System.out.println(""+rows.getString(1)+"\t"+rows.getString(2));
+            }
+            while (desc.next()) {
+                System.out.println(""+desc.getString(1));
+            }
+            mysql.CloseConnection();
+        }catch(Exception e){}
     }
 }
